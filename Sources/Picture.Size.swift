4981eb8f0cf09989241052ec8cb3c6ff0dcbf773
vaux <zero.griffin@gmail.com>
2021-11-30 19:35:50 +0100

    Adopt comparable

--- Sources/Picture.Size.swift
+++ Sources/Picture.Size.swift
@@ -1,13 +1,17 @@
 import Foundation
 
 extension Picture {
-    public struct Size: Hashable {
+    public struct Size: Hashable, Comparable {
         public let width: Int
         public let height: Int
         
         init(rotated: Bool, width: Int, height: Int) {
             self.width = rotated ? height : width
             self.height = rotated ? width : height
+        }
+        
+        public static func < (lhs: Self, rhs: Self) -> Bool {
+            lhs.width * lhs.height < rhs.width * rhs.height
         }
     }
 }
